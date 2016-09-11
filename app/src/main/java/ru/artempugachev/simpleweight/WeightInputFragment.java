5c0927ac790a4d774020adc732887d046cb55da0
Artem Pugachev <artempugachev1@gmail.com>
2016-09-11 22:06:37 +0300

    weight input base template

--- app/src/main/java/ru/artempugachev/simpleweight/WeightInputFragment.java
+++ app/src/main/java/ru/artempugachev/simpleweight/WeightInputFragment.java
@@ -1,10 +1,39 @@
 package ru.artempugachev.simpleweight;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by user on 10.09.2016.
  */
 
 public class WeightInputFragment extends Fragment {
+
+    public WeightInputFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.input_fragment, container, false);
+
+        return view;
+    }
+
+
 }
