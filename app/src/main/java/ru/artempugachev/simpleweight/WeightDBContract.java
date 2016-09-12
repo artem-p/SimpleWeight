9c1a33498922ba5857ad9aa6370f326762fbe083
Artem Pugachev <artempugachev1@gmail.com>
2016-09-12 23:50:55 +0300

    db schema

---
+++ app/src/main/java/ru/artempugachev/simpleweight/WeightDBContract.java
@@ -0,0 +1,14 @@
+package ru.artempugachev.simpleweight;
+
+import android.provider.BaseColumns;
+
+public final class WeightDBContract {
+    public WeightDBContract() {
+
+    }
+
+    public static abstract class WeightEntry implements BaseColumns {
+        public static final String TABLE_NAME = "weight";
+        public static final String COLUMN_NAME_TIME = "time";
+    }
+}
