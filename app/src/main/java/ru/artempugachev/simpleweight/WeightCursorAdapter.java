2d4053be507656ad8a643a55ea0184fa05b93eb3
Artem Pugachev <artempugachev1@gmail.com>
2016-09-15 23:57:41 +0300

    weight item and cursor adapter templates

---
+++ app/src/main/java/ru/artempugachev/simpleweight/WeightCursorAdapter.java
@@ -0,0 +1,26 @@
+package ru.artempugachev.simpleweight;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class WeightCursorAdapter extends CursorAdapter {
+    public WeightCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    // The newView method is used to inflate a new view and return it,
+    // you don't bind any data to the view at this point.
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.weight_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
